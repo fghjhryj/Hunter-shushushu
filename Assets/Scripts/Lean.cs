d1519e442ee22dc13d78284eb7752526f32fdeff
fghjhryj <74661583+fghjhryj@users.noreply.github.com>
2022-05-16 21:30:49 +0800

    彈珠軌跡與認識資料的基本四大類型

---
+++ Assets/Scripts/Lean.cs
@@ -0,0 +1,29 @@
+
+//單行註解 /*多行註解 藍色核心 白色名稱 綠色資料類型 
+using UnityEngine;
+//修飾詞  類別(藍圖) 類別名稱 繼承(物件導向程式設計 OOP 三大原則)
+public class Lean : MonoBehaviour
+{
+    // Lean類別成員區域
+
+    // 儲存資料方式:
+    // 欄位field
+    // 儲存各種資料
+    // 欄位語法
+    // 資料類型 欄位自訂名稱 結束符號
+    // 定義一筆整數資料 名稱叫做 hp
+    int hp;
+
+    // 資料類型:四大基本類型
+    // 整數:保存正負沒有小數點的資料 int
+    // 浮點數:保存正負有小數點的資料 float
+    // 字串:保存文字資訊 string
+    // 布林值:保存有、沒有 bool
+
+    int lv;
+    float exp;
+    string playerName;
+    bool hasCureSkill;
+
+}
+//非Lean類別成員區域
