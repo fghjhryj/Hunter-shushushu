ada2d0297c69842a9d9054e665fa211782430983
黑魚 <74661583+fghjhryj@users.noreply.github.com>
2022-06-29 21:36:30 +0800

    學習API

--- Assets/Scripts/LeanAPIStatic.cs
+++ Assets/Scripts/LeanAPIStatic.cs
@@ -19,5 +19,9 @@ public class LeanAPIStatic : MonoBehaviour
         print("圓周率 : " + Mathf.PI);
 
         // 2. 設定 set (Read Only 不能設定)
+        // 類別名稱.靜態屬性名稱  指定 值;
+        Screen.brightness = 0.5f;
+        Cursor.visible = false;
+
     }
 }
